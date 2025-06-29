270be6a0057dbc632bec8e27e3b97d901176b89c
jayradys <124400944+jayradys@users.noreply.github.com>
2025-06-29 15:58:28 -0400

    Generated code associated with pin configs.

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -107,8 +107,6 @@ void Error_Handler(void);
 #define RMII_TXD0_GPIO_Port GPIOG
 #define SWO_Pin GPIO_PIN_3
 #define SWO_GPIO_Port GPIOB
-#define LD2_Pin GPIO_PIN_7
-#define LD2_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
